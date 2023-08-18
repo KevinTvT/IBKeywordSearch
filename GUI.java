28538c9fef3de1ee172857f120e0d509927655b3
Kevin Nie <97629149+KevinTvT@users.noreply.github.com>
2023-08-18 15:34:28 -0700

    restart :) cause i'm torturing myself :)

--- GUI.java
+++ GUI.java
@@ -2,8 +2,29 @@ import java.awt.*;
 import java.util.*;
 import javax.swing.*;
 import java.io.*;
-import javax.swing.BorderFactory;
 
 public class GUI extends JFrame {
+    //the current subject folder
+    private static String home = System.getProperty("user.home");
+    private static File resourceFolder = new File(home+"/Downloads/IBKeywordSearchResources");
+    private static File currentFolder;
+
+    public GUI(){
+        //Set up the window
+        
+
+        //c.add(classDropDown);
+        
+    }
+
     
+
+    public static void main(String[] args){
+        GUI window = new GUI();
+        
+        window.setBounds(400, 400, 400, 200);
+        window.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        window.setResizable(false);
+        window.setVisible(true);
+    }
 }
